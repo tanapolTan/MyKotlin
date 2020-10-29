291bfea40273afbcd15e3bfb7d572a45c92e1f55
tanapol@emet <>
2020-10-29 22:00:00 +0700

    ccc

--- src/com/lek/reflection/MethodInfo.java
+++ src/com/lek/reflection/MethodInfo.java
@@ -3,9 +3,9 @@ package com.lek.reflection;
 import java.lang.reflect.Method;
 
 public class MethodInfo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Entity e = new Entity(10, "id");
-        Class clss = e.getClass();
+        Class<? extends Entity> clss = e.getClass();
         Method[] methods = clss.getMethods();
         for(Method method : methods) {
              System.out.println(method.getName());
@@ -15,5 +15,10 @@ public class MethodInfo {
         for(Method method : methods2) {
             System.out.println(method.getName());
         }
+
+        System.out.println("=================================");
+        Method med = clss.getMethod("setVal", int.class);
+        med.invoke(e, "15");
+        System.out.println(e.getVal());
     }
 }
