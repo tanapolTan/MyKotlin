bd001cefa94e2ea39a59cf584c4f761405b82962
Tanapol <tanapol@emetworks.com>
2020-11-30 22:52:43 +0700

    ffffff

--- src/HttpServer.kt
+++ src/HttpServer.kt
@@ -6,24 +6,22 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 
 fun main(args : Array<String>) {
-    val handler = object : HttpHandler {
-        override fun handle(p0 : HttpExchange?) : Unit {
-            val hx = p0!!
-            val reader: BufferedReader = BufferedReader(InputStreamReader(p0.requestBody))
-            reader.forEachLine {
-                println(it)
-            }
-
-            val headers = hx.getResponseHeaders()!!
-            headers.add("Content-Type", "text/plain")
-            hx.sendResponseHeaders(200, 0)
-
-            val out = hx.getResponseBody()!!.writer()
-            out.write("Hello, World!\n")
-            out.close()
-
-            println("Handled!")
+    val handler = HttpHandler { p0 ->
+        val hx = p0!!
+        val reader: BufferedReader = BufferedReader(InputStreamReader(p0.requestBody))
+        reader.forEachLine {
+            println(it)
         }
+
+        val headers = hx.getResponseHeaders()!!
+        headers.add("Content-Type", "text/plain")
+        hx.sendResponseHeaders(200, 0)
+
+        val out = hx.getResponseBody()!!.writer()
+        out.write("Hello, World!\n")
+        out.close()
+
+        println("Handled!")
     }
 
     val server = HttpServer.create(InetSocketAddress(8000), 0)!!
